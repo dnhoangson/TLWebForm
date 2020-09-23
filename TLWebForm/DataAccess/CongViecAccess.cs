1ad035f409a24f5663148953a2bff31fdce41075
dnhoangson <dnhoangson.danang@gmail.com>
2020-09-23 13:13:24 +0700

    Add MarkStatus

--- TLWebForm/DataAccess/CongViecAccess.cs
+++ TLWebForm/DataAccess/CongViecAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,56 @@ namespace TLWebForm.DataAccess
 {
     public class CongViecAccess
     {
-       
+       public void MarkStatus(string id, bool status)
+        {
+            string connectionString = DataAccess.Internal.DataAccess.GetConnectionString("TodoListDb");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = @"update CongViec " +
+                                "set Status = @status " +
+                                "where Id = @id";
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@status", status);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdateFinishDate(string id)
+        {
+            string connectionString = DataAccess.Internal.DataAccess.GetConnectionString("TodoListDb");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = @"update CongViec " +
+                                "set NgayKetThuc = GETDATE() " +
+                                "where Id = @id";
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void RemoveFinishDate(string id)
+        {
+            string connectionString = DataAccess.Internal.DataAccess.GetConnectionString("TodoListDb");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = @"update CongViec " +
+                                "set NgayKetThuc = NULL " +
+                                "where Id = @id";
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
