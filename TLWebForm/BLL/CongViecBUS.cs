9f4ac61241eeb9276706b3a9016a7c70ae8603ea
dnhoangson <dnhoangson.danang@gmail.com>
2020-09-23 12:40:18 +0700

    Added CongViecBUS and CongViecAccess

---
+++ TLWebForm/BLL/CongViecBUS.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TLWebForm.DataAccess;
+
+namespace TLWebForm.BLL
+{
+    public class CongViecBUS
+    {
+        
+    }
+}
